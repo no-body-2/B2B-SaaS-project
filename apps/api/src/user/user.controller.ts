68ae26df43391562436156b6eaabc92ee407c6fa
ai-chemist <utk8244@gmail.com>
2026-05-22 19:06:58 +0900

    feat(user): init User Module

---
+++ apps/api/src/user/user.controller.ts
@@ -0,0 +1,11 @@
+// apps/api/src/user/user.controller.ts
+
+/**
+ * User Controller
+ *
+ * @description
+ * User Module의 기능을 이용하기 위한 진입점 제공
+ *
+ * @author  <Nobody>
+ * @date 2026-05-22
+ */
---
+++ apps/api/src/user/user.service.ts
@@ -0,0 +1,11 @@
+// apps/api/src/user/user.service.ts
+
+/**
+ * User Service
+ *
+ * @description
+ * User 모듈의 Service
+ *
+ * @author  <Nobody>
+ * @date 2026-05-22
+ */
